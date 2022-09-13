37f8b290d1221f32c001792b89d3fbd145fe1ccb
Meg <megansindelar2023@u.northwestern.edu>
2022-09-13 15:53:16 -0500

    Updated nearest function and unit vector solving

--- rrt.py
+++ rrt.py
@@ -23,77 +23,48 @@ def random_configuration(D):
     return q_rand
 
 def nearest_vertex(q_rand, G):
-    for a in points:
-        distance = {}
-        diff = abs(np.subtract(a,q_rand))
-        print(diff)
-        distance.update(a = diff)
-
-    #print(distance)
-    dist = dict(sorted(distance.items(), key=lambda item:item[1]))
-    #print(dist)
-    q_near = list(dist.keys())[0]
-    print(q_near)
+    closestdist = 100
+    for i,a in enumerate(G):
+        dist = np.sqrt((q_rand[0]-a[0])**2 + (q_rand[1] - a[1])**2)
+        if dist < closestdist:
+            closestdist = dist
+            q_near = G[i]
     return q_near
+
                     
 def new_configuration(q_near, q_rand, delta):
     print(q_rand)
     print(q_near)
-    m = (q_rand[1]-q_near[1])/(q_rand[0]-q_near[0])
 
-    theta1 = np.atan(q_rand[0]-q_near[0])/(q_rand[1]-q_near[1])
-    theta2 = np.atan(q_rand[1]-q_near[1])/(q_rand[0]-q_near[0])
-    theta = [theta1, theta2]
-    theta.sort()
-    if theta[0] == theta1:
-        theta_real = theta1
-        theta1_true = 1
+    #unit vector
+    vector = np.subtract(q_rand, q_near)
+    vector_mag = np.sqrt((vector[0]**2 + vector[1]**2))
+    uv_x = vector[0]/vector_mag
+    uv_y = vector[1]/vector_mag
+    uv = [uv_x, uv_y]
+
+    x = delta*uv_x
+    y = delta*uv_y
+
+    if q_near[0] > 0 and q_near[1] > 0:
+        """First quadrant"""
+        x_new = q_near[0] + x
+        y_new = q_near[1] + y  
+
+    elif q_near[0] < 0 and q_near[1] > 0:
+        """Second quadrant"""
+        x_new = q_near[0] - x
+        y_new = q_near[1] + y
+
+    elif q_near[0] < 0 and q_near[1] < 0:
+        """Third quadrant"""
+        x_new = q_near[0] - x
+        y_new = q_near[1] - y
+
     else:
-        theta_real = theta2
-        theta1_true = 0
-
-    adj = abs(delta*np.cos(theta_real))
-    acr = abs(delta*np.sin(theta_real))
-
-    if theta1_true == 1 and q_near[0] > 0 and q_near[1] > 0:
-        """First quadrant, theta1"""
-        x_new = q_near[0] + acr
-        y_new = q_near[1] + adj
-
-    elif theta1_true == 0 and q_near[0] > 0 and q_near[1] > 0:
-        """First quadrant, theta2"""
-        x_new = q_near[0] + adj
-        y_new = q_near[1] + acr    
-
-    elif theta1_true == 1 and q_near[0] < 0 and q_near[1] > 0:
-        """Second quadrant, theta1"""
-        x_new = q_near[0] - acr
-        y_new = q_near[1] + adj
-
-    elif theta1_true == 0 and q_near[0] < 0 and q_near[1] > 0:
-        """Second quadrant, theta2"""
-        x_new = q_near[0] - adj
-        y_new = q_near[1] + acr
-
-    elif theta1_true == 1 and q_near[0] < 0 and q_near[1] < 0:
-        """Third quadrant, theta1"""
-        x_new = q_near[0] - acr
-        y_new = q_near[1] - adj
-
-    elif theta1_true == 0 and q_near[0] < 0 and q_near[1] < 0:
-        """Third quadrant, theta2"""
-        x_new = q_near[0] - adj
-        y_new = q_near[1] - acr
-
-    elif theta1_true == 1 and q_near[0] > 0 and q_near[1] < 0:
-        """Fourth quadrant, theta1"""
-        x_new = q_near[0] + acr
-        y_new = q_near[1] - adj
-
-    elif theta1_true == 0 and q_near[0] < 0 and q_near[1] < 0:
-        """Second quadrant, theta2"""
-        x_new = q_near[0] + adj
-        y_new = q_near[1] - acr
+        """Fourth quadrant"""
+        x_new = q_near[0] + x
+        y_new = q_near[1] - y
 
     q_new = [x_new, y_new]
 
@@ -104,16 +75,17 @@ def new_configuration(q_near, q_rand, delta):
 
     return q_new
         
-    
         
 
 parents = {}
 
-q_init = (50,50)
+q_init = [50,50]
 delta = 1
 K = 50
 D = [[0,100],[0,100]]
 
+diction = {1: [0,2], 2: [4,3]}
+print(diction)
 
 # lst = []
 # dista = {}
@@ -124,25 +96,30 @@ D = [[0,100],[0,100]]
 # print(dista)
 
 
-points = [q_init]
+G = [q_init]
 
-
-G = Graph(q_init, delta, K, D)
-q_rand = random_configuration(G.D)
-q_near = nearest_vertex(q_rand, G)
+node = Graph(q_init, delta, K, D)
+q_rand = random_configuration(node.D)
+#q_near = nearest_vertex(q_rand, G, points)
 #q_new = new_configuration(q_near, q_rand, G.delta)
 
 
 
 print(q_init)
 
-fig, ax = plt.subplots()
-ax.set_xlim(G.D[0][0],G.D[0][1])
-ax.set_ylim(G.D[1][0],G.D[1][1])
-seg1 = [q_init, (51,50)]
-seg2 = [(43, 44), (22, 60)]
-lc = LineCollection([seg1, seg2])
-ax.add_collection(lc)
-plt.show()
+# fig, ax = plt.subplots()
+# ax.set_xlim(node.D[0][0],node.D[0][1])
+# ax.set_ylim(node.D[1][0],node.D[1][1])
+# seg1 = [q_init, (51,50)]
+# seg2 = [(43, 44), (22, 60)]
+# lc = LineCollection([seg1, seg2])
+# ax.add_collection(lc)
+# plt.show()
 #plt.axes.Axes.set_xlim()
+
+
+
+
+
+
 
