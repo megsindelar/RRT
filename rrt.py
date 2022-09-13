427a397d76b31b97a0b81d6a6082731962f12ade
Meg <megansindelar2023@u.northwestern.edu>
2022-09-13 16:19:22 -0500

    Task 1: Simple RRT works!!

--- rrt.py
+++ rrt.py
@@ -3,12 +3,13 @@
 ##############################################
 
 from math import dist, hypot
+from re import X
 import matplotlib.pyplot as plt
 from matplotlib.collections import LineCollection
 import numpy as np
 
 
-class Graph():
+class Graph:
     def __init__(self,q_init, delta, K, D):
         self.q_init = q_init
         self.delta = delta
@@ -68,20 +69,24 @@ def new_configuration(q_near, q_rand, delta):
 
     q_new = [x_new, y_new]
 
-    points.append(q_new)
+    G.append(q_new)
 
-    """Child is the key, parent is the value in this dictionary"""
-    parents.update(q_new = q_near) 
+    """Parent is the q_near, child is the q_new"""
+    parents.append([q_near,q_new])
+
+    #"""Child is the key, parent is the value in this dictionary"""
+    #parents.update(q_new = q_near) 
+    
 
     return q_new
         
         
 
-parents = {}
+parents = []
 
 q_init = [50,50]
 delta = 1
-K = 50
+K = 500
 D = [[0,100],[0,100]]
 
 diction = {1: [0,2], 2: [4,3]}
@@ -97,25 +102,28 @@ print(diction)
 
 
 G = [q_init]
-
 node = Graph(q_init, delta, K, D)
-q_rand = random_configuration(node.D)
-#q_near = nearest_vertex(q_rand, G, points)
-#q_new = new_configuration(q_near, q_rand, G.delta)
+#segs = []
 
+while node.K > 0:
+    q_rand = random_configuration(node.D)
+    q_near = nearest_vertex(q_rand, G)
+    q_new = new_configuration(q_near, q_rand, node.delta)
+    node.K-=1
+    #segs.append()
 
+# for i,val in enumerate(parents):
+#     parent_dict = 
+# print(parents)
 
-print(q_init)
-
-# fig, ax = plt.subplots()
-# ax.set_xlim(node.D[0][0],node.D[0][1])
-# ax.set_ylim(node.D[1][0],node.D[1][1])
-# seg1 = [q_init, (51,50)]
-# seg2 = [(43, 44), (22, 60)]
-# lc = LineCollection([seg1, seg2])
-# ax.add_collection(lc)
-# plt.show()
-#plt.axes.Axes.set_xlim()
+fig, ax = plt.subplots()
+ax.set_xlim(node.D[0][0],node.D[0][1])
+ax.set_ylim(node.D[1][0],node.D[1][1])
+#seg1 = [q_init, (51,50)]
+#seg2 = [(43, 44), (22, 60)]
+lc = LineCollection(parents)
+ax.add_collection(lc)
+plt.show()
 
 
 
